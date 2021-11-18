512b8ecbc77c2455cfe17b62978189e5b31e84da
TeeRoberts <taylornroberts2009@gmail.com>
2021-11-17 20:50:54 -0600

    Started writing main game script, subscripts completed

--- game_play/main_game.py
+++ game_play/main_game.py
@@ -1,115 +1,176 @@
 import os
 import pygame
 import random
+import logging
+
 from items.creatures import *
+from items.players import Player
 from items.side_quest_items import *
-from items.riddles import *
+from game_methods.item_methods import *
+from game_methods.quest_methods import *
+from game_methods.riddle_methods import *
+
 
 pygame.font.init()
 
+# ToDo add logging to necessary functions
+# ToDo add spawn locations to all creatures in creature mapping dict
 
-'''  
-def run_side_quest(creature):
-        # enemy play
-        generate sq item ?
-        display instructions for quest (?.quest)
+logger = logging.getLogger('game_play_logs')
+logger.setLevel(logging.INFO)
+lh = logging.FileHandler('../logs/gameplay.log')
+logger.addHandler(lh)
 
-        Then:
-        in side quest we must spawn items and put them in a list
-        side_quest_item = ? this would be passed as parameter
+WIDTH, HEIGHT = 850, 775
+GAME_WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('The Riddlemaster')
 
-        side_quest_items = []
-        prev_used_locations = []
-        dupe_locations = False
-        num_of_items = ?.spawn
-
-        for i in range(1, num_of_items):
-            ?.item_spawn_location(WIDTH, HEIGHT)
-            item_location = (?.x_pos, ?.y_pos)
-            if item_location in prev_used_locations:
-                i -= 1
-                continue
-            side_quest_items.append(?)
-            prev_used_locations.append(item_location)
-
-        all this needs to be in a timed loop!!!
-        for enemy_ship in enemies:
-            enemy_ship.draw(GAME_WINDOW)
-
-        copy of list = enemies.copy()
-
-        while time is not 0:
-            for enemy in enemies_copy:
-                if enemy.collision(player1):
-                    player.pickup()
-                    player1.score += ?.points
-                    player1.purse += ?.value
-                    enemies.remove(enemy)
-
-        When time is 0, get length of list of thingies
-        if player got 0 display sad message
-        if player got some display not bad message
-        if player got all give happy message
-        have player give items to creature (for item in inventory, if item == side quest item, remove it)
+GAME_BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('../assets/', 'green_background2.png')), (WIDTH, HEIGHT))
 
 
-def startup_game():
-    # print welcome screen and ask if want to play
-    # if yes, display instructions, if no quit
-    # check first to see if won == true, if not, continue and play game
+def initialize_game():
+    title_font = pygame.font.SysFont('comicsans', 50)
+    run = True
+
+    while run:
+        logger.info('Game start screen loading...')
+        GAME_WINDOW.blit(GAME_BACKGROUND, (0, 0))
+        title_label = title_font.render('Click your mouse to begin', True, (255, 255, 255))
+        GAME_WINDOW.blit(title_label, (WIDTH / 2 - title_label.get_width() / 2, 325))
+
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                logger.info('Game session ended.')
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                logger.info('Game session starting...')
+                main_game()
+
+    pygame.quit()
 
 
-def main_play():
-    # game variables
+def main_game():
+    main_loop = True
+    playing = True
+    end_quest = False
+    sidequest_levels = [3, 6, 9, 12]
     level = 1
-    side_quest_levels = [3, 6, 9, 12]
-    
-    # creature variables
-    creature_x = 900
-    creature_y = 900
-    level_creature_map = {1: Basilisk(creature_x, creature_y), 2: Cerberus(creature_x, creature_y),
-                          3: Owl(creature_x, creature_y), 4: Minotaur(creature_x, creature_y),
-                          5: Unicorn(creature_x, creature_y), 6: Wyvern(creature_x, creature_y),
-                          7: Phoenix(creature_x, creature_y), 8: Griffin(creature_x, creature_y),
-                          9: Dryad(creature_x, creature_y), 10: Centaur(creature_x, creature_y),
-                          11: Sphinx(creature_x, creature_y), 12: GoldDragon(creature_x, creature_y)}
-    
-    # player variables
-    tries = 0
-    
-    # main riddle loop
-    while your lives are not 0 and you didn't quit
-        answeringRiddle = True
-        level_creature = level_creature_map[level] # should this be in the game loop?
-        spawn creature based on map
-        create riddle and right answers using the get riddle function on your creature
-        then use that info (riddle) to have your creature do intro and ask riddle
-        
-        create loop 
-        while answeringRiddle == True:
-            set up input so user can type in answer
-            store answer in string and add one to the tries
-            player_response = input(riddle) # how do we do user inputs in pygame
-            tries += 1
-            compare your answer and see if in right answers, then correctAnswer = true
-            pass this info to riddle answered correctly function
-            if correct answer is true and endquest is false, then display creature message and break loop
-            if correct answer false and endquest is false, display creature message and continue
-            if correct answer is false and endquest is true, display creature message and set game over break loop
-        
-        if game over then quit/break main loop
+
+    fps = 60
+    clock = pygame.time.Clock()
+    main_font = pygame.font.SysFont("comicsans", 45)
+
+    creature_spawn_x, creature_spawn_y = WIDTH - (resize_x * 2 + 15), HEIGHT - (resize_y * 2 + 15)
+    level_creature_mappings = {1: Basilisk(creature_spawn_x, creature_spawn_y),
+                               2: Cerberus(creature_spawn_x, creature_spawn_y),
+                               3: Owl(creature_spawn_x, creature_spawn_y),
+                               4: Minotaur(creature_spawn_x, creature_spawn_y),
+                               5: Unicorn(creature_spawn_x, creature_spawn_y),
+                               6: Wyvern(creature_spawn_x, creature_spawn_y),
+                               7: Phoenix(creature_spawn_x, creature_spawn_y),
+                               8: Griffin(creature_spawn_x, creature_spawn_y),
+                               9: Wyvern(creature_spawn_x, creature_spawn_y),
+                               10: Centaur(creature_spawn_x, creature_spawn_y),
+                               11: Sphinx(creature_spawn_x, creature_spawn_y),
+                               12: GoldDragon(creature_spawn_x, creature_spawn_y)}
+
+    user_player = Player('Tree Greenley', 25, HEIGHT - 110)
+    level_creature = level_creature_mappings[level]
+
+    def redraw_main_window():
+        logger.info('Drawing window...')
+        # set values for text variables
+        GAME_WINDOW.blit(GAME_BACKGROUND, (0, 0))
+        score = user_player.points
+        coins = user_player.purse
+        lives = user_player.lives
+
+        # add text variables to window
+        level_label = main_font.render(f'Level: {level}', True, (255, 255, 255))
+        lives_label = main_font.render(f'Lives: {lives}', True, (255, 255, 255))
+        score_label = main_font.render(f'Score: {score}', True, (255, 255, 255))
+        coins_label = main_font.render(f'Coins: {coins}', True, (255, 255, 255))
+
+        label_blocks = WIDTH/4
+
+        # display the variables
+        GAME_WINDOW.blit(level_label, (10, 10))
+        GAME_WINDOW.blit(lives_label, (label_blocks, 10))
+        GAME_WINDOW.blit(score_label, (label_blocks * 2, 10))
+        GAME_WINDOW.blit(coins_label, (label_blocks * 3, 10))
+
+        user_player.draw(GAME_WINDOW)
+        level_creature.draw(GAME_WINDOW)
+
+        pygame.display.update()  # update game window
+        logger.info('Window completed...')
+
+    while main_loop:
+        clock.tick(fps)
+        redraw_main_window()
+
+        while playing:
+            level_riddle, level_answers = get_riddle(level_creature)
+
+'''         
+            level_riddle, level_answers = get_riddle(level_creature)
+            display the riddles in a banner
+            have banner for user to input their response
+            player_answer = that input
             
-        if side quest level do side quest: put side level quest in separate function
-        if level in side_quest_levels:
-            run_side_quest()
-        then call that function
-        this will be the end of the main game loop
+            right_answer, end_quest, creature_response = is_riddle_answer_correct(user_player, tries, player_answer, riddle_answers, level_creature)
+            if end_quest:
+                break
+            else:
+                display creature_response
+                
+            if level in sidequest_levels:
+                run_side_quest(level, level_creature, user_player)
+            
+            level += 1
         
-        level += 1
+        if user_player.lives == 0 and end_quest:
+               ask user if they want to continue
+               if user_continue == 'y':
+                   player.lives = 3
+                   player.purse = 0
+                   player.points = 0
+                   continue
+               else:
+                   display end message
+                   break
+                   
+    quit the game completely
+    
+    
+def run_side_quest(level, creature, player):
+    banner0 = 'Use arrow keys to move'
+    side_quest_level_map = {3: Fireberries(), 6: GoldKey(), 9: WoodenStaff(), 12: DragonFang()}
+    side_quest_item = side_quest_level_map[level]
         
-        check space invaders for end of loop setup 
+    banner1 = creature.quest
+    display banner1 
+    user presses key and that goes away
+    
+    display banner0
+    
+    build screen/window for the side quest:
+    spawn the items 
+    sidequest_items_list = thingie
+    create and start the timer
+    
+    need to somehow check list each time to see if any items still left
+    remove item from screen if picked up
+    when all items are collected and timer still running stop
+    when timer runs out erase remaining items and stop
+    return to original level screen
+    
+    evaluate the players' performance and update their scores
+    sidequest_response = sidequest_completed(side_quest_item, sidequest_items_list, player, creature)
+    display sidequest_response
 '''
 
 
 if __name__ == '__main__':
-    startup_game()
+    initialize_game()
