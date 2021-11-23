109ed5ab97ba46b19cc1d3bc1e7f066055cdcd8a
TeeRoberts <taylornroberts2009@gmail.com>
2021-11-22 21:48:05 -0600

    Completed first two game screens and added display window functions plus new log

--- items/riddles.py
+++ items/riddles.py
@@ -1,16 +1,116 @@
-# TODO make list of riddles, where level number matches index. level number plus one and then match that
-# display riddle['question']
-# create lists for each level with multiple riddles in it
+# riddles and accepted answers
+# will need to convert user input to lower and strip last period
 
-riddles = [
-    {'question': 'Spawn', 'answer': 'Spawn', 'points': 44, 'worth': 44}
-]
+basilisk_riddles = ['Why can\'t the world ever come to an end?',
+                    'You live in a one story house made of red wood. What color are the stairs?',
+                    'What always ends everything?',
+                    'What has five wheels but only moves on four?']
+basilisk_answers = [['it is round', 'it\'s round'],
+                    ['there are no stairs', 'there\'s no stairs', 'no stairs'],
+                    ['g', 'the letter g'],
+                    ['a car', 'car', 'cars']]
+
+cerberus_riddles = ['What is full of holes but still holds water?',
+                    'What gets wet when drying?',
+                    'What seven letter word reads the same forwards as backwards?',
+                    'The more you take, the more you leave behind.']
+cerberus_answers = [['sponge', 'sponges', 'a sponge'],
+                    ['towels', 'a towel', 'towel'],
+                    ['racecar'],
+                    ['footsteps', 'foot steps']]
+
+owl_riddles = ['What gets bigger the more you take away?',
+               'What is caught, but never thrown?',
+               'I eat to live and drink to die, what am I?',
+               'What goes up but does not come down?']
+owl_answers = [['a hole', 'hole', 'holes'],
+               ['a cold', 'colds', 'cold', 'the flu', 'flu', 'a virus', 'virus'],
+               ['a fire', 'fires', 'fire'],
+               ['your age', 'my age', 'age']]
+
+minotaur_riddles = ['How many letters are in the alphabet?',
+                    'What goes up but never grows?',
+                    'What starts with a T, ends with a T, and has T in it?',
+                    'How do you divide four apples among six people?']
+minotaur_answers = [['11', 'eleven'],
+                    ['mountains', 'a mountain', 'the mountains'],
+                    ['a teapot', 'teapot'],
+                    ['applesauce', 'make applesauce']]
+
+unicorn_riddles = ['What can be touched but not felt?',
+                   'Take me out and scratch my head, and now it\'s black what once was red. What am I?',
+                   'What is black, and white, and read all over?',
+                   'What has two eyes but cannot see?']
+unicorn_answers = [['your heart', 'your heart.', 'the heart'],
+                   ['a match', 'match', 'matches'],
+                   ['newspaper', 'newspapers', 'a newspaper'],
+                   ['buttons', 'a button', 'button']
+                    ]
+
+wyvern_riddles = ['What has many keys but can\'t open a door.',
+                  'What comes once a minute, twice in a moment, but never in a thousand years?'
+                  'What travels around the world, but stays in one spot?',
+                  'Name a cage without a door, window, or hinges.']
+wyvern_answers = [['keyboard', 'a keyboard', 'keyboards', 'piano', 'a piano', 'pianos'],
+                  ['m', 'the letter m'],
+                  ['a stamp', 'stamps', 'stamp'],
+                  ['eggs', 'egg', 'an egg']]
+
+phoenix_riddles = ['Name something with fingers and thumbs that isn\'t living?',
+                   'Name a place where today comes before yesterday.',
+                   'Name something no one wants, but no one wants to lose when they get one.',
+                   'Name something with two hands and no arms.']
+phoenix_answers = [['a glove', 'glove', 'gloves'],
+                   ['dictionary', 'a dictionary', 'dictionaries'],
+                   ['lawsuits', 'lawsuit', 'a lawsuit'],
+                   ['watch', 'clock', 'a watch', 'a clock', 'watches', 'clocks']]
+
+griffin_riddles = ['I shave every day, but my beard never changes. Who am I?',
+                   'If your uncle\'s sister is not your aunt, what is she?',
+                   'Who has married many people but never been married?',
+                   'What has a head and a tail but no legs?']
+griffin_answers = [['a barber', 'barber'],
+                   ['your mother', 'my mother', 'mother', 'my mom', 'your mom', 'mom'],
+                   ['a preacher', 'a minister', 'a priest', 'priest', 'preacher', 'minister'],
+                   ['a coin', 'coin', 'coins']]
+
+dryad_riddles = ['Mary\'s mother had four children: April, May, and June. What was the fourth child\'s name?',
+                 'I speak without a mouth and hear with no ears. What am I?',
+                 'It flies without wings and cries without eyes. When it leads, darkness follows. What is it?',
+                 'What was never scared but became petrified?']
+dryad_answers = [['mary'],
+                 ['an echo', 'echo', 'echoes'],
+                 ['a cloud', 'cloud', 'clouds'],
+                 ['wood']]
+
+centaur_riddles = ['I shoot people for a living, but I don\'t kill anyone. Who am I?',
+                   'Mr. and Mrs. Dragon have seven daughters. Each daughter has one brother. How many Dragons are there?',
+                   'A man got caught out in the rain with no jacket and no umbrella. Yet his hair didn\'t get wet, why?',
+                   'There\'s one in every corner and two in every room.']
+centaur_answers = [['photographer', 'a photographer'],
+                   ['10', 'ten'],
+                   ['he was bald', 'he\'s bald', 'bald', 'he is bald'],
+                   ['o', 'the letter o']]
+
+sphinx_riddles = ['What has cities but no houses, forests but no trees, and water but no fish?',
+                  'What runs but never walks, has a mouth but doesn\'t talk, has a head that never weeps, and a bed but never sleeps?',
+                  'A storekeeper told his customer that his parrot "repeats everything it hears". The customer said good morning, but the parrot never responded. Why not?'
+                  'I am tall when I am young, and short when I am old. What am I?']
+sphinx_answers = [['a map', 'map', 'maps'],
+                  ['a river', 'river', 'rivers', 'a brook', 'brook', 'brooks', 'a creek', 'creek', 'creeks'],
+                  ['it is deaf', 'it\'s deaf', 'it was deaf','deaf', 'the bird is deaf', 'the bird was deaf', 'the bird\'s deaf'],
+                  ['a candle', 'a pencil', 'candle', 'pencil']]
+
+gold_dragon_riddles = ['You are in a room with a candle, a stove, and a lamp. You only have one match. Which do you light first?',
+                       'If you have one, you want to share it. But once you share it, you no longer have one. What is it?',
+                       'The person who sees it doesn\'t need it and the person who needs it doesn\'t see it. What is it?',
+                       'What goes on four legs in the morning, two at noon, and three in the evening?']
+gold_dragon_answers = [['the match', 'match', 'a match'],
+                       ['secrets', 'secret', 'a secret'],
+                       ['a coffin', 'coffin', 'coffins'],
+                       ['man', 'human', 'a human', 'a man', 'humans', 'men', 'women', 'a woman', 'woman']]
 
 
-'''Sphinx_riddles = {‘Level’: 0, 
-		‘Riddles’: [‘What goes on four legs in the morning, two in the day, and three at night?’,
-‘Name something that goes up but does not come down.’]
-		‘Answers’: [[‘Man’, ‘Men’, ‘Humans’, ‘Human’, ‘Human being’],
-[‘age’, ‘my age’, ‘your age’]] '''
+
 
 
